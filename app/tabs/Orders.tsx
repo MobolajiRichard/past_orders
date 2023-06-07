16c208e2c97f8707defd0523e73742aa7d7899ab
Mobolaji Richard <lernenplatz@gmail.com>
2023-06-07 16:53:05 +0100

    code updated

--- app/tabs/Orders.tsx
+++ app/tabs/Orders.tsx
@@ -1,9 +1,19 @@
-import { View, Text, SafeAreaView, StyleSheet, Pressable, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  Image,
+  FlatList,
+} from "react-native";
 import React from "react";
 import { Dimensions } from "react-native";
 import image from "../../constant/image";
+import Order from "../../components/Order";
 
 const Orders = () => {
+  const data = [1, 2, 3, 4, 5, 6];
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
@@ -11,7 +21,12 @@ const Orders = () => {
 
         <View style={styles.container2}>
           <View style={styles.imageContainer}>
-            <Image style={styles.image} resizeMode='contain' source={image.image1} alt='hero'/>
+            <Image
+              style={styles.image}
+              resizeMode="contain"
+              source={image.image1}
+              alt="hero"
+            />
           </View>
           <Text style={styles.name}>Angel R.</Text>
           <Text style={styles.orderInfo}>TV Mounting 2:00 pm</Text>
@@ -21,13 +36,25 @@ const Orders = () => {
           </View>
           <View style={styles.buttonContainer}>
             <Pressable style={[styles.button, { backgroundColor: "#D9D9D9" }]}>
-              <Text style={[styles.orderTime, {opacity:1}]}>Report Issue</Text>
+              <Text style={[styles.orderTime, { opacity: 1 }]}>
+                Report Issue
+              </Text>
             </Pressable>
             <Pressable style={[styles.button, { backgroundColor: "#58C0DB" }]}>
-              <Text style={[styles.orderTime, { color: "white", opacity:1 }]}>Done</Text>
+              <Text style={[styles.orderTime, { color: "white", opacity: 1 }]}>
+                Done
+              </Text>
             </Pressable>
           </View>
         </View>
+
+        <FlatList
+          data={data}
+          contentContainerStyle={{ flexGrow: 1, paddingHorizontal: 8 }}
+          renderItem={({ item }) => <Order />}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(order) => order}
+        />
       </View>
     </SafeAreaView>
   );
@@ -38,13 +65,13 @@ const styles = StyleSheet.create({
     flex: 1,
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
-    padding: 30,
+    paddingHorizontal: 30,
   },
   headerText: {
     fontSize: 28,
     fontWeight: "800",
     fontFamily: "raleway",
-    marginBottom:28
+    marginBottom: 28,
   },
   container2: {
     width: "100%",
@@ -52,7 +79,8 @@ const styles = StyleSheet.create({
     borderColor: "#d9d9d9",
     borderRadius: 15,
     paddingHorizontal: "8.6%",
-    paddingVertical:27
+    paddingVertical: 27,
+    marginBottom:32,
   },
   orderDetails: {
     display: "flex",
@@ -86,22 +114,22 @@ const styles = StyleSheet.create({
   orderPrice: {
     fontFamily: "mont_semibold",
     fontSize: 14,
-    opacity:0.5
+    opacity: 0.5,
   },
   orderTime: {
     fontFamily: "mont_medium",
     fontSize: 14,
-    opacity:0.4
+    opacity: 0.4,
   },
-  imageContainer:{
-    marginBottom:29,
-    marginTop:12,
-    borderRadius:15,
+  imageContainer: {
+    marginBottom: 29,
+    marginTop: 12,
+    borderRadius: 15,
   },
-  image:{
-    borderRadius:15,
-    width:'100%'
-  }
+  image: {
+    borderRadius: 15,
+    width: "100%",
+  },
 });
 
 export default Orders;
--- components/Order.tsx
+++ components/Order.tsx
@@ -1,12 +1,85 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+import image from "../constant/image";
+import Check from "../assets/icons/Check";
 
 const Order = () => {
   return (
-    <View>
-      <Text>Order</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={image.image2} resizeMode="contain" alt="image" />
+      </View>
+      <View style={styles.details}>
+        <Text style={styles.name}>Angel R.</Text>
+        <Text style={styles.title}>Furniture Assembly</Text>
+        <View style={styles.descContainer}>
+        <Text style={styles.desc}>
+          TV Mounting Apr 24  
+        </Text>
+        <View style={styles.dot}></View>
+        <Text style={styles.desc}>12:30 pm</Text>
+        </View>
+        
+        <Text style={styles.price}>$44.39/hr</Text>
+      </View>
+      <Check />
     </View>
-  )
-}
+  );
+};
 
-export default Order
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom:31
+  },
+  imageContainer: {
+    width: 84,
+    height: 72,
+    borderRadius: 15,
+    marginRight: 8,
+  },
+  image: {
+    borderRadius: 15,
+    width: "100%",
+  },
+  details: {
+    flex: 1,
+    marginRight:22
+  },
+  name: {
+    fontFamily: "mont_bold",
+    fontSize: 14,
+    marginBottom: 3,
+  },
+  title: {
+    fontFamily: "mont_medium",
+    fontSize: 14,
+  },
+  descContainer:{
+    flexDirection:'row',
+    alignItems:'center',
+  },
+  desc: {
+    fontFamily: "mont_medium",
+    fontSize: 14,
+    opacity: 0.5,
+    marginBottom: 3,
+  },
+  price: {
+    fontFamily: "mont_medium",
+    fontSize: 14,
+    opacity: 0.5,
+  },
+  dot: {
+    width: 5,
+    height: 5,
+    borderRadius: 100,
+    opacity: 0.5,
+    backgroundColor: "black",
+    marginHorizontal:4
+  },
+});
+
+export default Order;
